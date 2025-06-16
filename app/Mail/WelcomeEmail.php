739f08743f94c1790be207b12071f25388f952ec
@CML-ebedoya <ebedoya@cmlexports.com>
2025-06-16 04:33:47 -0400

    Enviar segundo correo después de la verificación del email

---
+++ app/Mail/WelcomeEmail.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function build()
+    {
+        return $this->subject('¡Bienvenido a GOLAZOBETS!')
+                    ->markdown('emails.emails.welcome');
+    }
+}
